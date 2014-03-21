fa17068f4816474e79959549d6e021330811d771
Owl <darkowl91@gmail.com>
2014-03-21 16:02:56 +0300

    Add SignIn SignUp

--- src/main/java/com/wash/mvc/controllers/AccountController.java
+++ src/main/java/com/wash/mvc/controllers/AccountController.java
@@ -1,7 +1,18 @@
 package com.wash.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class AccountController {
+public class AccountController extends BaseController {
+
+    @RequestMapping(value = "/signIn")
+    public String signIn() {
+        return "carWash.signIn";
+    }
+
+    @RequestMapping(value = "/signUp")
+    public String signUp() {
+        return "carWash.signUp";
+    }
 }
--- src/main/java/com/wash/mvc/controllers/BaseController.java
+++ src/main/java/com/wash/mvc/controllers/BaseController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class BaseController {
+public abstract class BaseController {
 
 	@RequestMapping(value="/", method = RequestMethod.GET)
-	public String startPage() {
+	public final String startPage() {
 		return "carWash.welcome";
 	}
 }
