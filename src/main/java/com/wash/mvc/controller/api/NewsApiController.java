0f8f881486a4e84ef7d47763fee1cd1de098fc43
Owl <darkowl91@gmail.com>
2014-04-29 12:12:25 +0300

    random news output

--- src/main/java/com/wash/mvc/controller/api/NewsApiController.java
+++ src/main/java/com/wash/mvc/controller/api/NewsApiController.java
@@ -1,5 +1,6 @@
 package com.wash.mvc.controller.api;
 
+import com.google.common.collect.Lists;
 import com.wash.model.advertisement.News;
 import com.wash.mvc.service.INewsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -20,6 +22,12 @@ public class NewsApiController {
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
     public List<News> newsList() {
-        return newsService.findAll();
+        List<News> all = newsService.findAll();
+        if (all.size() >= 2) {
+            Collections.shuffle(all);
+            return Lists.newArrayList(all.get(0), all.get(1));
+        } else {
+            return all;
+        }
     }
 }
