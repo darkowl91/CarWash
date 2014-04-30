a6d592c75af0a356525ef028fc5d5d6c5354768b
Ilya Kukushkin <ilya_kukushkin@epam.com>
2014-04-30 16:08:09 +0300

    Change encoding

--- src/main/java/com/wash/model/account/Type.java
+++ src/main/java/com/wash/model/account/Type.java
@@ -2,9 +2,9 @@ package com.wash.model.account;
 
 public enum Type {
 	
-	MOBILE("Мобильный"),
-	HOME("Домашний"),
-	WORK("Рабочий");
+	MOBILE("\u041C\u043E\u0431\u0438\u043B\u044C\u043D\u044B\u0439"),
+	HOME("\u0414\u043E\u043C\u0430\u0448\u043D\u0438\u0439"),
+	WORK("\u0420\u0430\u0431\u043E\u0447\u0438\u0439");
 	
 	private String value;
 	
